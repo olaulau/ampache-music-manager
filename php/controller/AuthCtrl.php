0e7bed825aacfd9b5782af553d3c9128a83ef614
olaulau <olaulau@gmail.com>
2025-08-17 19:10:38 +0200

    login & logout

--- php/controller/AuthCtrl.php
+++ php/controller/AuthCtrl.php
@@ -2,6 +2,8 @@
 namespace controller;
 
 use Base;
+use ErrorException;
+use model\User;
 use View;
 
 
@@ -22,8 +24,6 @@ class AuthCtrl extends Ctrl
 	
 	public static function loginGET (Base $f3, array $params, string $controller)
 	{
-		
-		
 		$view = new View();
 		echo $view->render('login.phtml');
 	}
@@ -32,7 +32,37 @@ class AuthCtrl extends Ctrl
 	public static function loginPOST (Base $f3, array $params, string $controller)
 	{
 		
+		$login = $f3->get("POST.login");
+		$password = $f3->get("POST.password");
+		if(empty($login) || empty($password)) {
+			throw new ErrorException("parameter problem");
+		}
 		
+		$user = new User()->findone(["username = ?", $login]);
+		if(empty($user)) {
+			sleep(3);
+			echo "user not found";
+			die;
+		}
+		
+		if ($user->password !== hash("sha256", $password)) {
+			sleep(3);
+			echo "password missmatch";
+			die;
+		}
+		
+		if($user->access !== 100) {
+			throw new ErrorException("insuffisant access");
+		}
+		
+		$f3->set("SESSION.auth.login", $login);
+		$f3->reroute("@homepage");
+	}
+	
+	
+	public static function logoutGET (Base $f3, array $params, string $controller)
+	{
+		$f3->clear("SESSION.auth.login");
 		$f3->reroute("@homepage");
 	}
 	
---
+++ php/model/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace model;
+
+class User extends Mdl
+{
+	
+	final $table = "user";
+	
+}
